3d46419a58f395fbecb206e8efa1191844662c57
Anderson <and.santos123@gmail.com>
2022-05-30 18:27:30 -0300

    Criando os elementos de forma dinâmica

---
+++ js/script.js
@@ -0,0 +1,108 @@
+// array movies
+const moviesAll = [
+  {
+    image: 'https://img.elo7.com.br/product/original/3FBA809/big-poster-filme-batman-2022-90x60-cm-lo002-poster-batman.jpg',
+    title: 'Batman',
+    rating: 9.2,
+    year: 2022,
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    isFavorited: false
+  },
+  {
+    image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/9/9b/Avengers_Endgame.jpg/250px-Avengers_Endgame.jpg',
+    title: 'Avengers',
+    rating: 9.2,
+    year: 2019,
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    isFavorited: true
+  },
+  {
+    image: 'https://upload.wikimedia.org/wikipedia/en/1/17/Doctor_Strange_in_the_Multiverse_of_Madness_poster.jpg',
+    title: 'Doctor Strange',
+    rating: 9.2,
+    year: 2022,
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    isFavorited: false
+  },
+]
+
+// id:movies-list
+const moviesList = document.querySelector('#movies-list')
+
+// create movie
+const createMovie = (movie) =>{ 
+  const {title, image, rating, year, description, isFavorited} = movie
+
+  // movie
+  const movieElement= document.createElement('div')
+  movieElement.classList.add('movie')
+  document.querySelector('#movies-list').appendChild(movieElement)
+
+  // movie image
+  const movieImage = document.createElement('div')
+  movieImage.classList.add('image')
+  
+  const imgSrc = document.createElement('img')
+  imgSrc.setAttribute('src', image, 'alt', title)
+  movieImage.appendChild(imgSrc)
+  movieElement.appendChild(movieImage)
+  
+  // movie title
+  const movieTitle = document.createElement('div')
+  movieTitle.classList.add('title')
+  
+  const h3 = document.createElement('h3')
+  h3.textContent = `${title} (${year})`
+  movieTitle.appendChild(h3)
+
+  // rating and favorites
+  const ratingFavorites = document.createElement('div')
+  ratingFavorites.classList.add('rating-favorites')
+  
+  // rating
+  const ratingElement = document.createElement('div')
+  ratingElement.classList.add('rating')
+  
+  const imgRatingElement = document.createElement('img')
+  imgRatingElement.setAttribute('src', 'images/star.svg', 'alt', 'Rating')
+
+  const valueRating = document.createElement('span')
+  valueRating.textContent = rating
+
+  ratingElement.appendChild(imgRatingElement)
+  ratingElement.appendChild(valueRating)
+  ratingFavorites.appendChild(ratingElement)
+
+  // favorites
+  const favoritesElement = document.createElement('div')
+  favoritesElement.classList.add('favorites')
+  
+  const imgFavoritesElement = document.createElement('img')
+  imgFavoritesElement.setAttribute('src', isFavorited ? 'images/heart-2.svg' : 'images/heart.svg', 'alt', 'Favorites')
+
+  const titleFavorite = document.createElement('span')
+  titleFavorite.textContent = "Favoritar"
+
+  favoritesElement.appendChild(imgFavoritesElement)
+  favoritesElement.appendChild(titleFavorite)
+  ratingFavorites.appendChild(favoritesElement)
+
+  movieTitle.appendChild(ratingFavorites)
+  movieElement.appendChild(movieTitle)
+
+  // description
+  const descriptionElement = document.createElement('div')
+  descriptionElement.classList.add('description')
+
+  const tagP = document.createElement('p')
+  tagP.textContent = description
+
+  descriptionElement.appendChild(tagP)
+  movieElement.appendChild(descriptionElement)
+}
+
+// load
+window.onload = () => {
+  moviesAll.forEach((movie) => createMovie(movie));
+}
+
